598c409d9a9ceb5f867701818725ce5faf489584
mamba <samarbaev36@gmail.com>
2024-05-30 20:38:37 +0300

    add #define for const

--- chapter_1/table_temputure_f_to_c.c
+++ chapter_1/table_temputure_f_to_c.c
@@ -1,19 +1,18 @@
 #include <stdio.h>
 
+#define LOWER 0
+#define UPPER 300
+#define STEP 20
+
 int main() {
 
-    float fahr, celsius;
-    int lower, upper, step;
+    float fahr,celsius; 
 
-    lower = 0;
-    upper = 300;
-    step = 20;
-
-    fahr = lower;
+    fahr = LOWER;
     printf("%3s\t%6s\n", "F", "C");
-    while (fahr <= upper) {
+    while (fahr <= UPPER) {
        celsius = 5.0 * (fahr - 32.0) / 9.0;
        printf("%3.0f\t%6.1f\n", fahr, celsius);
-       fahr = fahr + step;
+       fahr = fahr + STEP;
     }
 }
